23fd76b31b37fe1730fa54f15c4381a0232212fe
Yuqin Li <60296340+Uking33@users.noreply.github.com>
2021-05-09 10:36:53 -0400

    t

--- HWProducer.java
+++ HWProducer.java
@@ -2,7 +2,7 @@ import java.util.Properties;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
-public class Producer{
+public class HWProducer{
     public static void main(String[] args) throws Exception{	      
     // Check arguments length value
       if(args.length == 0){
