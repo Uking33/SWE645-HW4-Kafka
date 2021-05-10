f451227643f5ac88eb0ef538274080f940fd7ff3
Yuqin Li <60296340+Uking33@users.noreply.github.com>
2021-05-10 01:47:42 -0400

    t

--- producer.java
+++ producer.java
@@ -19,15 +19,15 @@ public class producer{
       //Assign localhost id
       props.put("bootstrap.servers", "10.43.100.131:9092");   
       //Set acknowledgements for producer requests.      
-      props.put("acks", "all");      
+      //props.put("acks", "all");      
       //If the request fails, the producer can automatically retry,
-      props.put("retries", 0);      
+      //props.put("retries", 0);      
       //Specify buffer size in config
-      props.put("batch.size", 16384);      
+      //props.put("batch.size", 16384);      
       //Reduce the no of requests less than 0   
-      props.put("linger.ms", 1);      
+      //props.put("linger.ms", 1);      
       //The buffer.memory controls the total amount of memory available to the producer for buffering.   
-      props.put("buffer.memory", 102400);      
+      //props.put("buffer.memory", 102400);      
       props.put("key.serializer", 
          "org.apache.kafka.common.serialization.StringSerializer");         
       props.put("value.serializer", 
