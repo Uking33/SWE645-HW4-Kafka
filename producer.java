72ea17d575883eacd55757012cc0184c10a71d5f
Yuqin Li <60296340+Uking33@users.noreply.github.com>
2021-05-10 07:12:42 -0400

    t

--- producer.java
+++ producer.java
@@ -1,14 +1,12 @@
+import java.util.List;
 import java.util.Properties;
 import org.apache.kafka.clients.producer.Producer;
-import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.clients.producer.RecordMetadata;
 
-import com.sun.org.slf4j.internal.Logger;
-import com.sun.org.slf4j.internal.LoggerFactory;
+import com.google.gson.Gson;
+
 public class producer{
-	final static Logger logger = LoggerFactory.getLogger(StudentBean.class);
     public static void main(String[] args) throws Exception{	      
     // Check arguments length value
       if(args.length == 0){
@@ -33,30 +31,12 @@ public class producer{
       Producer<String, String> producer = new KafkaProducer
          <String, String>(props);
       
-      ProducerRecord<String, StudentBean> record = 
-    		  new ProducerRecord<String, StudentBean>("first_topic", "message from java" + Integer.toString(0));
-      producer.send(record, new Callback() {
-           public void onCompletion(RecordMetadata recordMetadata, Exception e) {
-        	   if (e == null) {
-                    // the record is sent successfully        		   
-                    logger.debug("Received new metadata. \n" +
-                            "Topic: " + recordMetadata.topic() + "\n" +
-                            "Partition: " + recordMetadata.partition() + "\n" +
-                            "Offset: " + recordMetadata.offset() + "\n" +
-                            "Timestamp: " + recordMetadata.timestamp());
-                } else {
-                    logger.error("Error while producing", e);
-                }
-
-           }
-      });
-
-      /*System.out.println("Start");
-      for(int i = 0; i < 10; i++) {
-          producer.send(new ProducerRecord<StudentBean>(topicName, 
-             Integer.toString(i), Integer.toString(i)));  
-          System.out.println("send"+i);
-      }*/
+      StudentBean stu = new StudentBean();
+	  String json = new Gson().toJson(stu);
+      
+      ProducerRecord<String, String> record = 
+    		  new ProducerRecord<String, String>(topicName, stu.getUid(), json);
+      producer.send(record);
       
       System.out.println("Message sent successfully");
       producer.close();
