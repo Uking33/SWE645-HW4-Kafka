23660bbb1297a6a418adffbcd9ff37784d3cce5e
Yuqin Li <60296340+Uking33@users.noreply.github.com>
2021-05-10 00:20:39 -0400

    t

--- consumer.java
+++ consumer.java
@@ -1,55 +1,45 @@
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.common.serialization.StringDeserializer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import java.time.Duration;
-import java.util.Arrays;
 import java.util.Properties;
+import java.util.Arrays;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 
 public class consumer {
-    private static String bootstrapServers = "server:9092";
-    private static String groupId = "my-forth-app";
-    private static String topic = "first_topic";
-
-
-    public static void main(String[] args) {
-        Logger logger = LoggerFactory.getLogger(consumer.class);
-
-        /**
-         * create Consumer properties
-         *
-         * Properties are available in official document:
-         * https://kafka.apache.org/documentation/#consumerconfigs
-         *
-         */
-        Properties properties = new Properties();
-        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
-        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-
-        // create consumer
-        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(properties);
-
-        // subscribe consumer to our topic(s)
-        consumer.subscribe(Arrays.asList(topic));
-
-        // poll for new data
-        while(true){
-            ConsumerRecords<String, String> records =
-                    consumer.poll(Duration.ofMinutes(100));
-
-            for(ConsumerRecord record : records){
-                logger.info("Key: " + record.key() + "\t" + "Value: " + record.value() +
-                        "Topic: " + record.partition() + "\t" + "Partition: " + record.partition()
-               );
-
-            }
-        }
-
-    }
+   public static void main(String[] args) throws Exception {
+      if(args.length == 0){
+         System.out.println("Enter topic name");
+         return;
+      }
+      //Kafka consumer configuration settings
+      String topicName = args[0].toString();
+      Properties props = new Properties();
+      
+      props.put("bootstrap.servers", "localhost:9092");
+      props.put("group.id", "test");
+      props.put("enable.auto.commit", "true");
+      props.put("auto.commit.interval.ms", "1000");
+      props.put("session.timeout.ms", "30000");
+      props.put("key.deserializer", 
+         "org.apache.kafka.common.serializa-tion.StringDeserializer");
+      props.put("value.deserializer", 
+         "org.apache.kafka.common.serializa-tion.StringDeserializer");
+      KafkaConsumer<String, String> consumer = new KafkaConsumer
+         <String, String>(props);
+      
+      //Kafka Consumer subscribes list of topics here.
+      consumer.subscribe(Arrays.asList(topicName));
+      
+      //print the topic name
+      System.out.println("Subscribed to topic " + topicName);
+      int i = 0;
+      
+      while (true) {
+         ConsumerRecords<String, String> records = consumer.poll(100);
+         for (ConsumerRecord<String, String> record : records)
+         
+         // print the offset,key and value for the consumer records.
+         System.out.printf("offset = %d, key = %s, value = %s\n", 
+            record.offset(), record.key(), record.value());
+      }
+   }
 }
