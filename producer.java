df61faa38e104c86717686c4175f86412e3dd57f
Yuqin Li <60296340+Uking33@users.noreply.github.com>
2021-05-10 02:42:06 -0400

    Update producer.java

--- producer.java
+++ producer.java
@@ -16,7 +16,7 @@ public class producer{
       // create instance for properties to access producer configs   
       Properties props = new Properties();
       props.put("producer.type", "async");
-      props.put("bootstrap.servers", "localhost:9092");
+      props.put("bootstrap.servers", "172.331.77.95:9092");
       //props.put("metadata.broker.list", "10.43.100.131:9092");
       //Set acknowledgements for producer requests.      
       //props.put("acks", "all");      
