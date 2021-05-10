e6ca32b30e43131a20ee8b478752c879f33715e9
Yuqin Li <60296340+Uking33@users.noreply.github.com>
2021-05-10 02:33:32 -0400

    Update producer.java

--- producer.java
+++ producer.java
@@ -15,20 +15,13 @@ public class producer{
       
       // create instance for properties to access producer configs   
       Properties props = new Properties();
-      
-      //Assign localhost id
+      props.put("producer.type", "async");
       props.put("bootstrap.servers", "10.43.100.131:9092");
       //props.put("metadata.broker.list", "10.43.100.131:9092");
       //Set acknowledgements for producer requests.      
       props.put("acks", "all");      
       //If the request fails, the producer can automatically retry,
-      props.put("retries", 0);      
-      //Specify buffer size in config
-      props.put("batch.size", 16384);      
-      //Reduce the no of requests less than 0   
-      //props.put("linger.ms", 1);      
-      //The buffer.memory controls the total amount of memory available to the producer for buffering.   
-      props.put("buffer.memory", 102400);      
+      props.put("retries", 0);            
       props.put("key.serializer", 
          "org.apache.kafka.common.serialization.StringSerializer");         
       props.put("value.serializer", 
